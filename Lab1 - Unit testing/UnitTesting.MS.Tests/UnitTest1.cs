a1bf12737417236dfcd965178b2d4ec98c68083b
Ilya Meyta <Grizley.medved@gmail.com>
2019-02-17 20:33:53 +0500

    Lab1 solution initialize

---
+++ Lab1 - Unit testing/UnitTesting.MS.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting.MS.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+		}
+	}
+}
